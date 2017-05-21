58a5d97eb1fc90772c583e263e22223095ca86a5
harveylee <luappaul88@hotmail.com>
2017-05-21 15:41:17 +0800

    animate tiem

--- js/anim.js
+++ js/anim.js
@@ -2,17 +2,17 @@ $(function() {
 
     //-----------------ScrollReveal----------------
     // Changing the defaults
-    window.sr = ScrollReveal({ reset: true, duration: 800 });
+    window.sr = ScrollReveal({duration: 800 });
     // reset: true,
 
     // Customizing a reveal set
     sr.reveal('.avatar-section', { delay: 500 });
-    sr.reveal('.talkbubble ', { delay: 1000 });
-    sr.reveal('code', { origin: 'top', delay: 1500 });
+    sr.reveal('.talkbubble ', { delay: 500 });
+    sr.reveal('code', { origin: 'top', delay: 800 });
 
     sr.reveal('.hgroup', { origin: 'left', distance: '100px', delay: 500, });
-    sr.reveal('.duties', { origin: 'top', distance: '50px', delay: 1000 });
+    sr.reveal('.duties', { origin: 'top', distance: '50px', delay: 500 });
 
-    sr.reveal('.workItem', { delay: 500 });
-    sr.reveal('#contactForm', { mobile: false, origin: 'right', distance: '100px', delay: 1000, duration: 1000 });
+    sr.reveal('.workItem', { delay: 300 });
+    sr.reveal('#contactForm', { mobile: false, origin: 'right', distance: '100px', delay:800, duration: 1000 });
 });
